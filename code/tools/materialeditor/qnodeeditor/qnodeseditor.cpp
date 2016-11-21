97ee9e1848389c76d983c3cccc821e423e245d52
Xin Huang <coderhx@gmail.com>
2016-11-21 14:24:11 +0800

    Just a copy with https://bitbucket.org/RedAgito/nebula-3-game-engine

---
+++ code/tools/materialeditor/qnodeeditor/qnodeseditor.cpp
@@ -0,0 +1,246 @@
+/* Copyright (c) 2012, STANISLAW ADASZEWSKI
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of STANISLAW ADASZEWSKI nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL STANISLAW ADASZEWSKI BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+#include "qnodeseditor.h"
+
+#include <QGraphicsScene>
+#include <QEvent>
+#include <QGraphicsSceneMouseEvent>
+
+#include "qneport.h"
+#include "qneconnection.h"
+#include "qneblock.h"
+
+#include <ogdf/layered/SugiyamaLayout.h>
+#include <ogdf/layered/FastHierarchyLayout.h>
+
+QNodesEditor::QNodesEditor(QObject *parent) :
+	QObject(parent)
+{
+	conn = 0;
+}
+
+void QNodesEditor::install(QGraphicsScene *s)
+{
+	s->installEventFilter(this);
+	scene = s;
+}
+
+QGraphicsItem* QNodesEditor::itemAt(const QPointF &pos)
+{
+	QList<QGraphicsItem*> items = scene->items(QRectF(pos - QPointF(1, 1), QSize(3, 3)));
+
+	foreach(QGraphicsItem * item, items)
+	if (item->type() > QGraphicsItem::UserType)
+		return item;
+
+	return 0;
+}
+
+bool QNodesEditor::eventFilter(QObject *o, QEvent *e)
+{
+	QGraphicsSceneMouseEvent *me = (QGraphicsSceneMouseEvent*) e;
+
+	switch ((int) e->type())
+	{
+	case QEvent::GraphicsSceneMousePress:
+		{
+			switch ((int) me->button())
+			{
+			case Qt::LeftButton:
+				{
+					QGraphicsItem *item = itemAt(me->scenePos());
+					if (item && item->type() == QNEPort::Type)
+					{
+						conn = new QNEConnection(0, scene);
+						conn->setPort1((QNEPort*) item);
+						conn->setPos1(item->scenePos());
+						conn->setPos2(me->scenePos());
+						conn->updatePath();
+
+						return true;
+					}
+					else if (item && item->type() == QNEBlock::Type)
+					{
+						/* if (selBlock)
+							selBlock->setSelected(); */
+						// selBlock = (QNEBlock*) item;
+					}
+					break;
+				}
+			case Qt::RightButton:
+				{
+					QGraphicsItem *item = itemAt(me->scenePos());
+					if (item && (item->type() == QNEConnection::Type || item->type() == QNEBlock::Type))
+						delete item;
+					// if (selBlock == (QNEBlock*) item)
+					// selBlock = 0;
+					break;
+				}
+			}
+		}
+	case QEvent::GraphicsSceneMouseMove:
+		{
+			if (conn)
+			{
+				conn->setPos2(me->scenePos());
+				conn->updatePath();
+				return true;
+			}
+			break;
+		}
+	case QEvent::GraphicsSceneMouseRelease:
+		{
+			if (conn && me->button() == Qt::LeftButton)
+			{
+				QGraphicsItem *item = itemAt(me->scenePos());
+				if (item && item->type() == QNEPort::Type)
+				{
+					QNEPort *port1 = conn->port1();
+					QNEPort *port2 = (QNEPort*) item;
+
+					if (port1->block() != port2->block() && port1->isOutput() != port2->isOutput() && !port1->isConnected(port2))
+					{
+						conn->setPos2(port2->scenePos());
+						conn->setPort2(port2);
+						conn->updatePath();
+						conn = 0;
+						return true;
+					}
+				}
+
+				delete conn;
+				conn = 0;
+				return true;
+			}
+			break;
+		}
+	}
+	return QObject::eventFilter(o, e);
+}
+
+void QNodesEditor::save(QDataStream &ds)
+{
+	foreach(QGraphicsItem * item, scene->items())
+	if (item->type() == QNEBlock::Type)
+	{
+		ds << item->type();
+		((QNEBlock*) item)->save(ds);
+	}
+
+	foreach(QGraphicsItem * item, scene->items())
+	if (item->type() == QNEConnection::Type)
+	{
+		ds << item->type();
+		((QNEConnection*) item)->save(ds);
+	}
+}
+
+void QNodesEditor::load(QDataStream &ds)
+{
+	scene->clear();
+
+	QMap<quint64, QNEPort*> portMap;
+
+	while (!ds.atEnd())
+	{
+		int type;
+		ds >> type;
+		if (type == QNEBlock::Type)
+		{
+			QNEBlock *block = new QNEBlock(0, scene);
+			block->load(ds, portMap);
+		}
+		else if (type == QNEConnection::Type)
+		{
+			QNEConnection *conn = new QNEConnection(0, scene);
+			conn->load(ds, portMap);
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+void
+QNodesEditor::autoLayout()
+{
+	using namespace ogdf;
+	Graph graph;
+	// setup graph
+	QMap<NodeElement*, QNEBlock*> nodeMap;
+	foreach(QGraphicsItem * item, scene->items())
+	{
+		if (item->type() == QNEBlock::Type)
+		{
+			NodeElement* node = graph.newNode();
+			item->setData(QNEBlock::Type, qVariantFromValue((void*)node));
+			nodeMap[node] = (QNEBlock*)item;
+		}
+	}
+	foreach(QGraphicsItem * item, scene->items())
+	{
+		if (item->type() == QNEConnection::Type)
+		{
+			QNEConnection* connection = (QNEConnection*)item;
+			NodeElement* node1 = (NodeElement*)connection->port1()->block()->data(QNEBlock::Type).value<void*>();
+			NodeElement* node2 = (NodeElement*)connection->port2()->block()->data(QNEBlock::Type).value<void*>();
+			graph.newEdge(node1, node2);
+		}
+	}
+	// node size
+	GraphAttributes graphAttr(graph,
+	                          GraphAttributes::nodeGraphics | GraphAttributes::edgeGraphics |
+	                          GraphAttributes::nodeLabel | GraphAttributes::nodeColor |
+	                          GraphAttributes::edgeColor | GraphAttributes::edgeStyle |
+	                          GraphAttributes::nodeStyle | GraphAttributes::nodeTemplate);
+	NodeElement* node;
+	forall_nodes(node, graph)
+	{
+		QNEBlock* item = nodeMap[node];
+		graphAttr.width(node) = item->getHeight();
+		graphAttr.height(node) = item->getWidth();
+	}
+
+	// compute layout
+	SugiyamaLayout layout;
+	//layout.setRanking(new OptimalRanking);
+	//layout.setCrossMin(new GreedyInsertHeuristic );
+
+	FastHierarchyLayout* ohl = new FastHierarchyLayout;
+	ohl->layerDistance(30);
+	ohl->nodeDistance(25);
+	//ohl->weightBalancing(0.8);
+	layout.setLayout(ohl);
+
+	layout.call(graphAttr);
+
+	// update node position
+	forall_nodes(node, graph)
+	{
+		double x = graphAttr.x(node);
+		double y = graphAttr.y(node);
+		QNEBlock* item = nodeMap[node];
+		item->setPos(y, x);
+	}
+}
